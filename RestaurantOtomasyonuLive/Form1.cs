29161a065789cffe124585608247d680e565cff9
Bedirhan ihtiyar <122042131+AceGladd@users.noreply.github.com>
2025-04-25 00:28:32 +0300

    Revert "Arayüz geliştirmeleri, syntax düzenlemeleri"

--- RestaurantOtomasyonuLive/Form1.cs
+++ RestaurantOtomasyonuLive/Form1.cs
@@ -10,27 +10,33 @@ using System.Windows.Forms;
 
 namespace RestaurantOtomasyonuLive
 {
-    public partial class StartScreen : Form
+    public partial class Form1 : Form
     {
-        public StartScreen()
+        public Form1()
         {
             InitializeComponent();
         }
 
-        private void btn_login_Click(object sender, EventArgs e) //Login butonu kontrol kısmı
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
         {
             if (sqlMethods.login(txt_mail.Text, txt_password.Text))
             {
-                MessageBox.Show("Hoş geldiniz");
-                MainMenu mainMenu = new MainMenu();
-                mainMenu.Show();
+                MessageBox.Show("hoş geldiniz");
+                Form2 home = new Form2();
+                home.Show();
                 this.Hide();
             }
+
             else
                 MessageBox.Show("Hatalı kullanıcı adı ya da şifre.");
         }
 
-        private void btn_signUp_Click(object sender, EventArgs e) //SignUp butonu - Kayıt olma kısmı kontrolü
+        private void btn_signUp_Click(object sender, EventArgs e)
         {
             SignUpForm signForm = new SignUpForm();
             signForm.Show();
